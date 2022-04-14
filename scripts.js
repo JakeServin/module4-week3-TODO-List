30c781d77391b8eb28285d752a489ffe68aa4cad
JakeServin <jakeservin@gmail.com>
2022-04-14 11:17:22 -0700

    fixed errors

--- scripts.js
+++ scripts.js
@@ -4,43 +4,7 @@ let pendingTasksWrapper = document.getElementById('pending-tasks-wrapper');
 let completedTasksWrapper = document.getElementById('completed-tasks-wrapper');
 let taskCounter = 1;
 let completed = false;
-let icon = document.createElement('img');
-let newTask = document.createElement('div');
-let taskContentDiv = document.createElement('div');
-let removeButton = document.createElement('div');
 
-function createPendingTask() {
-    icon.addEventListener('click', () => {
-        pendingTasksWrapper.removeChild(newTask);
-        icon.setAttribute('src', './images/check-square.svg');
-        removeButton.removeEventListener('click', () => { })
-        removeButton.addEventListener('click', () => {
-            completedTasksWrapper.removeChild(newTask);
-        })
-        icon.removeEventListener('click', () => { });
-        icon.addEventListener('click', () => {
-            createCompletedTask();
-        })
-    })
-    pendingTasksWrapper.appendChild(newTask);
-}
-
-function createCompletedTask() {
-    icon.addEventListener('click', () => {
-        pendingTasksWrapper.removeChild(newTask);
-        icon.setAttribute('src', './images/check-square.svg');
-        removeButton.removeEventListener('click', () => {})
-        removeButton.addEventListener('click', () => {
-            completedTasksWrapper.removeChild(newTask);
-        })
-        icon.removeEventListener('click', () => { });
-        icon.addEventListener('click', () => {
-            createPendingTask();
-        })
-    })
-    newTask.appendChild(icon);
-    pendingTasksWrapper.appendChild(newTask);
-}
 
 // function setCheck() {
 //     if (completed == false) {
@@ -86,26 +50,11 @@ addButton.addEventListener('click', () => {
         return;
     }
 
-    // let newTask = document.createElement('div');
+    let newTask = document.createElement('div');
     newTask.className = "taskBox align-center";
     newTask.setAttribute('id', `task-${taskCounter}`)
 
-    
-
-    // let taskContentDiv = document.createElement('div');
-    taskContentDiv.className = "col taskContent";
-    taskContentDiv.innerText = `${inputTask.value}`;
-
-    // let removeButton = document.createElement('div');
-    removeButton.setAttribute('type', 'button');
-    removeButton.setAttribute('id', `task-${taskCounter}`);
-    removeButton.setAttribute('class', 'col-3 small');
-    removeButton.innerText = "Remove";
-    removeButton.addEventListener('click', () => {
-        pendingTasksWrapper.removeChild(newTask);
-    })
-
-    
+    let icon = document.createElement('img');
     icon.setAttribute('src', './images/square.svg');
     icon.setAttribute('type', 'button');
     icon.setAttribute('height', '32px');
@@ -127,12 +76,23 @@ addButton.addEventListener('click', () => {
         })
         completedTasksWrapper.appendChild(newTask);
     })
+
+    let taskContentDiv = document.createElement('div');
+    taskContentDiv.className = "col taskContent";
+    taskContentDiv.innerText = `${inputTask.value}`;
+
+    let removeButton = document.createElement('div');
+    removeButton.setAttribute('type', 'button');
+    removeButton.setAttribute('id', `task-${taskCounter}`);
+    removeButton.setAttribute('class', 'col-3 small');
+    removeButton.innerText = "Remove";
+    removeButton.addEventListener('click', () => {
+        pendingTasksWrapper.removeChild(newTask);
+    })
     newTask.appendChild(icon);
     newTask.appendChild(taskContentDiv);
     newTask.appendChild(removeButton);
     pendingTasksWrapper.appendChild(newTask);
-    
-
     inputTask.value = '';
     taskCounter++; // Clear input field
     })
